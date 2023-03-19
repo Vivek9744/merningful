580d0d4c50e1b797f3747de3c427fb3c038b9296
Raman01010010 <rmnprjrrr@gmail.com>
2023-03-19 15:38:44 +0530

    Updated outlook smtp server to gmail smtp server

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,7 +2,8 @@ const nodemailer=require("nodemailer")
 const sendEmail=async(subjects,message,send_to,send_from,reply_to)=>{
 
     const transporter =nodemailer.createTransport({
-        host:"smtp-mail.outlook.com",
+/*
+       host:"smtp-mail.outlook.com",
         port:"587",
         auth:{
             user:"rmnprj@hotmail.com",
@@ -11,10 +12,20 @@ const sendEmail=async(subjects,message,send_to,send_from,reply_to)=>{
         tls:{
             rejectUnauthorized:false,
         }
+        */
+        host:"smtp.gmail.com",
+        port:"587",
+        auth:{
+            user:"collegeconnect121@gmail.com",
+            pass:'gvkptbzibjbjxdij',
+        },
+        tls:{
+            rejectUnauthorized:false,
+        }
 
     })
     const options={
-        from:"rmnprj@hotmail.com",
+        from:"collegeconnect121@gmail.com",
         to:send_to,
         replyTo:reply_to,
         subject:"One time password for College Connect",
