38ee4455130c9133958b23e224530c41bc85a1c3
Raman01010010 <rmnprjrrr@gmail.com>
2024-03-31 10:26:50 +0530

    backend url fix

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
-const URL="http://localhost:8003"
+//const URL="http://localhost:8003"
 //const URL="https://collegeconnect1.onrender.com"
-//const URL="https://a1-w3yj.onrender.com"
+const URL="https://college-connect.azurewebsites.net"
 export const addUser=async (data)=>{
     try{
         return await axios.post(`${URL}/register`,data)
